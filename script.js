9ba3a4720d38ca6731013063ee05d1a95aa04144
omgaXD <sladekop@gmail.com>
2023-11-05 20:31:23 +0600

    Add omga_reverse anim

--- script.js
+++ script.js
@@ -181,7 +181,94 @@ class fa {
 
         }
 
-    }   
+    }  
+    static omga_reverse() {
+        let rectWidth = settings.divsPerRow;
+        let rectHeight = settings.divsPerCol;
+        let x = 0;
+        let y = 0;
+        let dir = false;
+        const changeDir = true;
+        while (1 < rectWidth && 1 < rectHeight) {
+            // ######
+            // #    #
+            // ######
+            // iterate through the rectangle as through a cycle
+            // swap every element with (x, y), except for (x, y) itself
+            
+            let curX = x+1;
+            let curY = y;
+            const w = rectWidth;
+            const h = rectHeight;
+            let dx = 1, dy = 0; 
+            if (!dir) {
+                dx = 0; dy = 1; curX = x; curY = y+1;
+            }
+            while (curX != x || curY != y) {
+                swapElements(div(x+1, y+1), div(curX+1, curY+1));
+
+                // Move to the next position
+                curX += dx;
+                curY += dy;
+                if (dir) {
+                    // Change direction if necessary
+                    if (curX === w+x) {
+                        dx = 0;
+                        dy = 1;
+                        curX--;
+                        curY++;
+                    } else if (curX === -1+x) {
+                        dx = 0;
+                        dy = -1;
+                        curX++;
+                        curY--;
+                    } else if (curY === h+y) {
+                        dx = -1;
+                        dy = 0;
+                        curX--;
+                        curY--;
+                    } else if (curY === -1+y) {
+                        dx = 1;
+                        dy = 0;
+                        curX++;
+                        curY++;
+                    }
+                } else {
+                    // Change direction if necessary
+                    if (curX === w+x) {
+                        dx = 0;
+                        dy = -1;
+                        curX--;
+                        curY--;
+                    } else if (curX === -1+x) {
+                        dx = 0;
+                        dy = 1;
+                        curX++;
+                        curY++;
+                    } else if (curY === h+y) {
+                        dx = 1;
+                        dy = 0;
+                        curX++;
+                        curY--;
+                    } else if (curY === -1+y) {
+                        dx = -1;
+                        dy = 0;
+                        curX--;
+                        curY++;
+                    }
+                }
+            }
+
+
+            x++;
+            y++;
+            rectWidth -= 2;
+            rectHeight -= 2;
+            if (changeDir) dir = !dir;
+
+        }
+
+    } 
 }
 function animate(applyChange) {
     // For every element, get the current rect, then move it to the next position
@@ -239,7 +326,7 @@ function swapElements(el1, el2) {
 
 
 // Queue of animations
-const animQueue = [fa.omga, fa.shuffle];
+const animQueue = [fa.omga, fa.omga_reverse];
 let i = 0;
 
 setInterval(() => {
