f314a9890bdc999c681c01d0d081d78da385eeba
omgaXD <sladekop@gmail.com>
2023-11-08 23:01:08 +0600

    Add thinking with portals feature

--- slide2.js
+++ slide2.js
@@ -3,6 +3,9 @@
  */
 const slide2El = document.querySelector('#reflow_square_of_squares');
 
+
+const button = document.querySelector('#fs2_animate');
+
 function insertFlexGrid(side, parent, className, wh, margin) {
     parent.style.width = `${side * (wh + margin * 2) + margin*2}px`;
     parent.style.height = `${side * (wh + margin * 2) + margin*2}px`;
@@ -19,4 +22,24 @@ function insertFlexGrid(side, parent, className, wh, margin) {
     }
 }
 
-insertFlexGrid(10, slide2El, "square", 25, 2.5); 
+insertFlexGrid(40, slide2El, "square", 3, 1.5); 
+
+const slide2El2 = slide2El.cloneNode(true);
+slide2El.parentElement.appendChild(slide2El2);
+slide2El2.id = "reflow_square_of_squares2";
+slide2El2.style.bottom = `${slide2El.parentElement.clientHeight + slide2El.clientHeight}px`;
+slide2El2.style.left = "-400px"
+
+
+button.addEventListener('click', () => {
+    slide2El.style.transition = "bottom 1s ease-in-out"
+slide2El2.style.transition = "bottom 1s ease-in-out"
+
+
+    const bot = (+slide2El.style.bottom.slice(0, -2));
+    slide2El.style.bottom = `${bot-100}px`;
+    
+    const bot2 = (+slide2El2.style.bottom.slice(0, -2));
+    slide2El2.style.bottom = `${bot2-100}px`;
+});
+
